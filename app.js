7ac59ec7d54fe359547a8e5a294916c8ccbebffa
Tom Chomiak <tom.chomiak@gmail.com>
2016-05-18 14:10:38 -0500

    Load env variables locally

---
+++ app.js
@@ -0,0 +1,17 @@
+/*
+ * Require dependencies
+ */
+var dotenv = require('dotenv');
+dotenv.load();
+
+/*
+ * Load env variables
+ */
+var SENDGRID_API_KEY = process.env.SENDGRID_API_KEY;
+var SENDGRID_TEMPLATE_ID = process.env.SENDGRID_TEMPLATE_ID;
+var DEFAULT_CSV_INPUT_PATH = process.env.DEFAULT_CSV_INPUT_PATH;
+var DEFAULT_CSV_SKIP_HEADERS = process.env.DEFAULT_CSV_SKIP_HEADERS;
+var DEFAULT_CSV_FIRST_NAME_COL = process.env.DEFAULT_CSV_FIRST_NAME_COL;
+var DEFAULT_CSV_EMAIL_COL = process.env.DEFAULT_CSV_EMAIL_COL;
+var DEFAULT_CSV_PROJECT_COL = process.env.DEFAULT_CSV_PROJECT_COL;
+var DEFAULT_CSV_URL_COL = process.env.DEFAULT_CSV_URL_COL;
