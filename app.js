4d2ff951ffb8b4af5f00e20f5fea5aec2c7a4a43
Tom Chomiak <tom.chomiak@gmail.com>
2016-05-18 14:13:21 -0500

    Test send Sendgrid

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 /*
- * Require dependencies
+ * Load env variables
  */
 var dotenv = require('dotenv');
 dotenv.load();
-
-/*
- * Load env variables
- */
 var SENDGRID_API_KEY = process.env.SENDGRID_API_KEY;
 var SENDGRID_TEMPLATE_ID = process.env.SENDGRID_TEMPLATE_ID;
 var DEFAULT_CSV_INPUT_PATH = process.env.DEFAULT_CSV_INPUT_PATH;
@@ -15,3 +11,21 @@ var DEFAULT_CSV_FIRST_NAME_COL = process.env.DEFAULT_CSV_FIRST_NAME_COL;
 var DEFAULT_CSV_EMAIL_COL = process.env.DEFAULT_CSV_EMAIL_COL;
 var DEFAULT_CSV_PROJECT_COL = process.env.DEFAULT_CSV_PROJECT_COL;
 var DEFAULT_CSV_URL_COL = process.env.DEFAULT_CSV_URL_COL;
+
+
+/*
+ * Initialze Sendgrid
+ */
+var sendgrid  = require('sendgrid')(SENDGRID_API_KEY);
+
+var payload   = {
+  to      : 'to@example.com',
+  from    : 'from@other.com',
+  subject : 'Saying Hi',
+  text    : 'This is my first email through SendGrid'
+}
+
+sendgrid.send(payload, function(err, json) {
+  if (err) { console.error(err); }
+  console.log(json);
+});
